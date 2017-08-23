8e17ca2b9bea48e05717b6c88387ae46453bee55
John Shearer <john@ultranaut.com>
2017-08-23 12:02:03 -0400

    Revert Defy Media ad units back to OpenX
    Undoing commit b4b6235

--- templates/protostar/partials/ads/300x250btf.php
+++ templates/protostar/partials/ads/300x250btf.php
@@ -1,1 +1,28 @@
-<script type="text/javascript" data-placement="11320599" src="//pubportal.brkmd.com/tms/dtv451ef9b2213e749189a245aebb81eb655.js"></script>
+<!--/* OpenX Asynchronous JavaScript tag */-->
+
+<script type="text/javascript">
+  var MR_Mid_Params = {ybot_slot:"MR_Mid", ybot_size:"", ybot_cpm:""};
+  try{
+  MR_Mid_Params = yieldbot.getSlotCriteria('MR_Mid');
+  }catch(e){/*ignore*/}
+</script>
+
+<div id="537216142_300x250BTF" style="width:300px;height:250px;margin:0;padding:0">
+  <noscript>
+    <iframe id="b0027e8ad2" name="b0027e8ad2" src="//junemedia-d.openx.net/w/1.0/afr?auid=537216142&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="300" height="250">
+      <a href="//junemedia-d.openx.net/w/1.0/rc?cs=b0027e8ad2&cb=INSERT_RANDOM_NUMBER_HERE" >
+        <img src="//junemedia-d.openx.net/w/1.0/ai?auid=537216142&cs=b0027e8ad2&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt="">
+      </a>
+    </iframe>
+  </noscript>
+</div>
+<script type="text/javascript">
+  var OX_ads = OX_ads || [];
+  OX_ads.push({
+    slot_id: "537216142_300x250BTF",
+    auid: "537216142",
+    vars: {"ybot_slot":MR_Mid_Params.ybot_slot, "ybot_size": MR_Mid_Params.ybot_size, "ybot_cpm": MR_Mid_Params.ybot_cpm}
+  });
+</script>
+
+<script type="text/javascript" src="//junemedia-d.openx.net/w/1.0/jstag"></script>
